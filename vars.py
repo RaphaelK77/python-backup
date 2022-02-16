03e8783379b78f7b2b4424f59b1b5920e436bdb7
Raphael <raphael.kandler@student.uibk.ac.at>
2022-02-16 16:13:46 +0100

    Updated version

--- vars.py
+++ vars.py
@@ -18,7 +18,7 @@ loaded_config = None
 loaded_config_file = None
 loaded_config_filename = ""
 short_config_desc_len = 30
-current_version = "v1.5.0"
+current_version = "v1.5.1"
 remaining_time = -1
 time_update_timer = 0
 
